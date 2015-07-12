b61ce8d7db28fabd979c570a8e844e21a590d87d
Dennis Li <dennis.li.92@gmail.com>
2015-07-11 22:23:43 -0700

    Simple spectrum analyzer

--- modules/entry.js
+++ modules/entry.js
@@ -1,10 +1,10 @@
 import React from '../bower_components/react/react.js';
 import $ from '../bower_components/jquery/dist/jquery.js';
+import _ from '../bower_components/underscore/underscore.js'
 
 let audioContext = new (window.AudioContext || window.webkitAudioContext)(); // define audio context
 
-let oscillator = audioContext.createOscillator();
-oscillator.frequency.value = 17000;
+let oscillator = null;
 
 // let biquadFilter = audioContext.createBiquadFilter();
 // biquadFilter.type = biquadFilter.BANDPASS;
@@ -12,52 +12,61 @@ oscillator.frequency.value = 17000;
 // biquadFilter.Q.value = biquadFilter.frequency.value / (17000 - 13000);
 
 let fft = audioContext.createAnalyser();
+fft.fftSize = 1024;
+
+let nyquistFreq = audioContext.sampleRate / 2;
+let freqPerIndex = nyquistFreq / fft.frequencyBinCount;
+
 let gfx = null;
 
 let App = React.createClass({
 
   statics: {
-    setupMicrophone() {
-      navigator.getUserMedia = navigator.getUserMedia ||
-        navigator.webkitGetUserMedia ||
-        navigator.mozGetUserMedia ||
-        navigator.msGetUserMedia;
-
-      window.URL = window.URL || window.webkitURL;
+    dbAtFrequency(freqBuffer, freq) {
+      return freqBuffer[this.indexForFrequency(freq)];
     },
+
+    indexForFrequency(freq) {
+      return Math.round(freq / freqPerIndex);
+    },
+
+    frequencyForIndex(index) {
+      return freqPerIndex * index;
+    }
+  },
+
+  setupMicrophone() {
+    navigator.getUserMedia = navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia ||
+      navigator.msGetUserMedia;
+
+    window.URL = window.URL || window.webkitURL;
+
+    navigator.getUserMedia({audio: true}, 
+      this.processStream,
+      error => console.error(error)
+    );
   },
 
   componentDidMount() {
-    App.setupMicrophone();
+    this.setupMicrophone();
 
-    navigator.getUserMedia(
-      {
-        audio: {
-          'mandatory': {
-              'googEchoCancellation': 'false',
-              'googAutoGainControl': 'false',
-              'googNoiseSuppression': 'false',
-              'googHighpassFilter': 'false'
-          },
-          optional: []
-        }
-      },
-
-      this.processStream,
-
-      error => console.error(error)
-    );
-
-    this.setupOscillator();
+    this.updateOscillator(15000);
   },
 
-  setupOscillator() {
+  updateOscillator(freq) {
+    if (oscillator) {
+      oscillator.stop();
+    }
+
+    oscillator = audioContext.createOscillator();
+    oscillator.frequency.value = freq;
     oscillator.connect(audioContext.destination);
     oscillator.start()
   },
 
   processStream(stream) {
-    // let audio = this.refs['audio'].getDOMNode();
     let out = audioContext.createBufferSource();
 
     let source = audioContext.createMediaStreamSource(stream);
@@ -76,14 +85,22 @@ let App = React.createClass({
 
     gfx.clearRect(0,0,800,600); 
     gfx.fillStyle = 'gray'; 
-    gfx.fillRect(0,0,800,600); 
-     
-    var data = new Uint8Array(audioContext.sampleRate); 
-    fft.getByteFrequencyData(data); 
+    let freqBuffer = new Uint8Array(fft.frequencyBinCount); 
+    fft.getByteFrequencyData(freqBuffer); 
+
+    let data = _(freqBuffer)
+      .chain()
+      .map((value, index) => ({value, freq: App.frequencyForIndex(index)}))
+      .sortBy(obj => obj.value)
+      .value();
+
+    console.log(_(data).last())
+
+
     gfx.fillStyle = 'red'; 
 
-    for(var i=0; i<data.length; i++) { 
-      gfx.fillRect(100 + i*4, 100 + 256 - data[i] * 2, 3, 100); 
+    for(var i=0; i< freqBuffer.length; i++) { 
+      gfx.fillRect(100 + i, 100 + 256 - freqBuffer[i] * 2, 3, 100); 
     } 
   },
 
